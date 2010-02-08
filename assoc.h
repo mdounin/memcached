2d805534983b5baaf9f1842373fd219e3c4021b6
Maxim Dounin <mdounin@mdounin.ru>
2010-02-08 21:22:58 +0300

    repcached 2.2

--- assoc.h
+++ assoc.h
@@ -5,3 +5,6 @@ int assoc_insert(item *item);
 void assoc_delete(const char *key, const size_t nkey);
 void do_assoc_move_next_bucket(void);
 uint32_t hash( const void *key, size_t length, const uint32_t initval);
+#ifdef USE_REPLICATION
+char *assoc_key_snap(int *n);
+#endif /*USE_REPLICATION*/
--- replication.h
+++ replication.h
@@ -1,6 +1,6 @@
 #ifndef MEMCACHED_REPLICATION_H
 #define MEMCACHED_REPLICATION_H
-#define REPCACHED_VERSION "2.1"
+#define REPCACHED_VERSION "2.2"
 #include <netdb.h>
 
 enum CMD_TYPE {
